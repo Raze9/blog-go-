647e0baeb93f67f15683691489db9985187115f7
genshin impact <87218053+Raze9@users.noreply.github.com>
2023-03-08 15:43:39 +0800

    Delete blog2 directory

--- blog2/service/UserService.go
+++
@@ -1,8 +0,0 @@
-package service
-
-type UserService struct {
-	NickName string `form:"nick_name" json:"nick_name"`
-	UserName string `form:"user_name" json:"user_name"`
-	Password string `form:"password" json:"password"`
-	Key      string `form:"key" json:"key"` // 前端判断
-}
